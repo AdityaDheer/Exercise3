ae4e9ad084c5ab0db9e83969a49923370edcccdc
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-09-26 01:52:45 -0400

    code for head bobbing and sprinting

--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    int moveSpeed = 10; // how fast the player moves
-    float lookSpeedX = 3; // left/right mouse sensitivity
-    float lookSpeedY = 3; // up/down mouse sensitivity
-    int jumpForce = 300; // ammount of force applied to create a jump
 
-    public Transform camTrans; // a reference to the camera transform
-    float xRotation;
-    float yRotation;
+    [SerializeField] public int initialMoveSpeed = 4; // how fast the player moves
+    public int moveSpeed = 4;
+    [SerializeField] public int sprintSpeedMultiplier = 2;
+
+    [SerializeField] int jumpForce = 300; // ammount of force applied to create a jump
     Rigidbody _rigidbody;
+
+    //float xRotation;
+    //float lookSpeedY = 3;
+
+    float yRotationForMovement;
+    float lookSpeedX = 3;
+    
 
     //The physics layers you want the player to be able to jump off of. Just dont include the layer the palyer is on.
     public LayerMask groundLayer;
@@ -23,18 +28,13 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-#if UNITY_WEBGL && !UNITY_EDITOR
-        lookSpeedX *= .65f; //WebGL has a bug where the mouse has higher sensitibity. This compensates for the change. 
-        lookSpeedY *= .65f; //.65 is a rough guess based on testing in firefox.
-#endif
         _rigidbody = GetComponent<Rigidbody>(); // Using GetComponent is expensive. Always do it in start and chache it when you can.
-        Cursor.lockState = CursorLockMode.Locked; // Hides the mouse and locks it to the center of the screen.
     }
 
     void FixedUpdate()
     {
         //Creates a movement vector local to the direction the player is facing.
-        Vector3 moveDir = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxis("Horizontal"); // Use GetAxisRaw for snappier but non-analogue  movement
+        Vector3 moveDir = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal"); // Use GetAxisRaw for snappier but non-analogue  movement
         moveDir *= moveSpeed;
         moveDir.y = _rigidbody.velocity.y; // We dont want y so we replace y with that the _rigidbody.velocity.y already is.
         _rigidbody.velocity = moveDir; // Set the velocity to our movement vector
@@ -46,15 +46,23 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        xRotation -= Input.GetAxis("Mouse Y") * lookSpeedY;
-        yRotation += Input.GetAxis("Mouse X") * lookSpeedX;
-        xRotation = Mathf.Clamp(xRotation, -90, 90); //Keeps up/down head rotation realistic
-        camTrans.localEulerAngles = new Vector3(xRotation, 0, 0);
-        transform.eulerAngles = new Vector3(0, yRotation, 0);
+        yRotationForMovement += Input.GetAxis("Mouse X") * lookSpeedX;
+        //xRotation -= Input.GetAxis("Mouse Y") * lookSpeedY;
 
+        transform.eulerAngles = new Vector3(0, yRotationForMovement, 0);
+        
         if (grounded && Input.GetButtonDown("Jump")) //if the player is on the ground and press Spacebar
         {
             _rigidbody.AddForce(new Vector3(0, jumpForce, 0)); // Add a force jumpForce in the Y direction
+        }
+
+        if (grounded && Input.GetButton("Sprint"))
+        {
+            moveSpeed = initialMoveSpeed * sprintSpeedMultiplier;
+        }
+        else
+        {
+            moveSpeed = initialMoveSpeed;
         }
     }
 }
