6750a9bdf8805ba4d37c7e847951f1f21133e2fb
Nicholas Dell'Aquilo <nickdellaquilo@protonmail.com>
2022-09-29 20:10:41 -0400

    adjust pickup code, add audio trigger prefab

--- Assets/Code/PickUpObjective.cs
+++ Assets/Code/PickUpObjective.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PickUpObjective : MonoBehaviour
 {
-    public GameObject scoreText;
-    public int score = 0;
     public AudioSource pickupSound;
 
     void OnTriggerEnter(Collider other)
     {
         pickupSound.Play();
-        score ++;
-        scoreText.GetComponent<Text>().text = score.ToString();
+        UpdateScore.score ++;
+        Destroy(gameObject);
     }
 }
